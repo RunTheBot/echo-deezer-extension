3fbb736bd6940eb9e3cd12e556f7f4fe9a914b80
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2024-10-23 14:35:55 +0200

    Unsynced Lyrics Support

--- ext/src/main/java/dev/brahmkshatriya/echo/extension/clients/DeezerLyricsClient.kt
+++ ext/src/main/java/dev/brahmkshatriya/echo/extension/clients/DeezerLyricsClient.kt
@@ -5,6 +5,7 @@ import dev.brahmkshatriya.echo.common.models.Lyric
 import dev.brahmkshatriya.echo.common.models.Lyrics
 import dev.brahmkshatriya.echo.common.models.Track
 import dev.brahmkshatriya.echo.extension.DeezerApi
+import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.int
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
@@ -19,12 +20,17 @@ class DeezerLyricsClient(private val api: DeezerApi) {
             val trackObject = dataObject["track"]!!.jsonObject
             val lyricsObject = trackObject["lyrics"]!!.jsonObject
             val lyricsId = lyricsObject["id"]?.jsonPrimitive?.content ?: ""
-            val linesArray = lyricsObject["synchronizedLines"]!!.jsonArray
-            val lyrics = linesArray.map { lineObj ->
-                val line = lineObj.jsonObject["line"]?.jsonPrimitive?.content ?: ""
-                val start = lineObj.jsonObject["milliseconds"]?.jsonPrimitive?.int ?: 0
-                val end = lineObj.jsonObject["duration"]?.jsonPrimitive?.int ?: 0
-                Lyric(line, start.toLong(), start.toLong() + end.toLong())
+            val lyrics = if (lyricsObject["synchronizedLines"] != JsonNull) {
+                val linesArray = lyricsObject["synchronizedLines"]!!.jsonArray
+                linesArray.map { lineObj ->
+                    val line = lineObj.jsonObject["line"]?.jsonPrimitive?.content ?: ""
+                    val start = lineObj.jsonObject["milliseconds"]?.jsonPrimitive?.int ?: 0
+                    val end = lineObj.jsonObject["duration"]?.jsonPrimitive?.int ?: 0
+                    Lyric(line, start.toLong(), start.toLong() + end.toLong())
+                }
+            } else {
+                val lyricsText = lyricsObject["text"]!!.jsonPrimitive.content
+                listOf(Lyric(lyricsText, 0, Long.MAX_VALUE))
             }
             listOf(Lyrics(lyricsId, track.title, lyrics = lyrics))
         } catch (e: Exception) {
